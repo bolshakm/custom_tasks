abb9724d11e47a9ffdc79f16e2d72ec047dd48bb
Bolshak, Maksym <maksym.bolshak@mckesson.com>
2022-01-30 18:18:44 +0200

    added def test

---
+++ src/test/java/custom/level1/task2/RunnerTask2Test.java
@@ -0,0 +1,18 @@
+package custom.level1.task2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RunnerTask2Test {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void main() {
+		assertTrue(true);
+	}
+}
