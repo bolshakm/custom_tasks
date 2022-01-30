16471ca06d03dc0faba46457530f9167599f36da
Anton Kalenskiy <anton_kalenskiy@ukr.net>
2022-01-30 18:48:08 +0200

    add solution for task 1

---
+++ src/main/java/custom/level1/task1/RunnerTask1.java
@@ -0,0 +1,17 @@
+package custom.level1.task1;
+
+public class RunnerTask1 {
+    public static void main(String[] args) {
+        int a = 1;
+        short myShort = 3287;
+        long myLong = 43543654;
+        double myDouble = 237.48;
+        float meFloat = 214.43f;
+        char d = 'a';
+        boolean c = true;
+        byte myByte = 100;
+        String f = "Java cool";
+        System.out.println("It is an int variable and value is " + a + "");
+    }
+
+}
