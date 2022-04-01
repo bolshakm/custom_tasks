93ba1a40288b68460db2bb811e1977d57be09e8f
Anton Kalenskiy <anton_kalenskiy@ukr.net>
2022-04-01 20:32:48 +0300

    add solution for task 2,3,4,5;

--- src/main/java/custom/level1/task2/RunnerTask2.java
+++ src/main/java/custom/level1/task2/RunnerTask2.java
@@ -1,38 +1,39 @@
 package custom.level1.task2;
 
 public class RunnerTask2 {
-	//todo. Add implementation for method isEvenNumber. Method should return true if number is even and false if isn't.
-	// * Implement method isOddNumber. Method should return true if number is odd and false if isn't.
-	// Test it like it was tested in isEvenNumber case.
-	// ** Define is number odd using method isEvenNumber.
+    //todo. Add implementation for method isEvenNumber. Method should return true if number is even and false if isn't.
+    // * Implement method isOddNumber. Method should return true if number is odd and false if isn't.
+    // Test it like it was tested in isEvenNumber case.
+    // ** Define is number odd using method isEvenNumber.
 
-	//todo Додати реалізацію для методу isEvenNumber. Метод повинен повертати true якщо число є парне та false, якщо ні.
-	// * Написати новий метод, який матеме назву isOddNumber. Метод повинен повертати true, якщо число є непарне та false, якщо парне.
-	// Додати перевірку нового метода(isOddNumber) аналогічну перевірці методу isEvenNumber.
-	// ** Використай метод isEvenNumber в методі isOddNumber для визначення парності числа.
+    //todo Додати реалізацію для методу isEvenNumber. Метод повинен повертати true якщо число є парне та false, якщо ні.
+    // * Написати новий метод, який матеме назву isOddNumber. Метод повинен повертати true, якщо число є непарне та false, якщо парне.
+    // Додати перевірку нового метода(isOddNumber) аналогічну перевірці методу isEvenNumber.
+    // ** Використай метод isEvenNumber в методі isOddNumber для визначення парності числа.
 
-	public static void main(String[] args) {
-		System.out.println("One is even and it is a " + isEvenNumber(1));
-		System.out.println("Two is even and it is a " + isEvenNumber(2));
-		System.out.println("Three is even and it is a " + isEvenNumber(3));
-		System.out.println("Four is even and it is a " + isEvenNumber(4));
+    public static void main(String[] args) {
+        System.out.println("One is even and it is a " + isEvenNumber(1));
+        System.out.println("Two is even and it is a " + isEvenNumber(2));
+        System.out.println("Three is even and it is a " + isEvenNumber(3));
+        System.out.println("Four is even and it is a " + isEvenNumber(4));
 
-		System.out.println();
+        System.out.println();
 
-		System.out.println("One is odd and it is a " + isOddNumber(1));
-		System.out.println("Two is odd and it is a " + isOddNumber(2));
-		System.out.println("Three is odd and it is a " + isOddNumber(3));
-		System.out.println("Four is odd and it is a " + isOddNumber(4));
+        System.out.println("One is odd and it is a " + isOddNumber(1));
+        System.out.println("Two is odd and it is a " + isOddNumber(2));
+        System.out.println("Three is odd and it is a " + isOddNumber(3));
+        System.out.println("Four is odd and it is a " + isOddNumber(4));
 
-	}
+    }
 
-	private static boolean isEvenNumber(int number) {
-if(number%2==0) return true;
-else return false;
-	}
+    private static boolean isEvenNumber(int number) {
+		return number % 2 == 0;
+
+    }
 
 
-	private static boolean isOddNumber(int number) {
-		if (number % 2 == 0) return false;
-		else return true;
-	}}
+    private static boolean isOddNumber(int number) {
+		return !isEvenNumber(number);
+
+    }
+}
--- src/main/java/custom/level1/task3/RunnerTask3.java
+++ src/main/java/custom/level1/task3/RunnerTask3.java
@@ -1,58 +1,57 @@
 package custom.level1.task3;
 
 public class RunnerTask3 {
-	//todo. Add implementation for method defineStringType if string length less then 5 characters(char) method should return "small",
-	// if string length more then 5 char but less then 10 char "medium", if string length more then 10 char "big".
-	// Define size for a, b, c, d vars and print result to console ex:"test is a small string!"
-	// ** Implement method compareStrings method should get two strings in parameters and print result of comparing
-	// ex: "String test less then string test2"
+    //todo. Add implementation for method defineStringType if string length less then 5 characters(char) method should return "small",
+    // if string length more then 5 char but less then 10 char "medium", if string length more then 10 char "big".
+    // Define size for a, b, c, d vars and print result to console ex:"test is a small string!"
+    // ** Implement method compareStrings method should get two strings in parameters and print result of comparing
+    // ex: "String test less then string test2"
 
-	//todo. Додати реалізацію для метода defineStringType метод повинен повертатти String в якому буде записано розмір стрінги.
-	// Якщо розмір стрінги менше 5 символів метод повинен повертнути "small", якщо від 5 до 10 символів то "medium", а якщо більше 10 символів то "big".
-	// Визнач розміри для змінних a, b, c, d, та виведи результат в консль у вигляді:"lol is a small string!"
-	// ** Створи нивий метод compareStrings, метод повинен отримувати 2 стрінги в якості аргументів методу і надрукувати результат порівняння
-	// наприклад: "String test less then string test2"
-	public static void main(String[] args) {
-		String a = "testing";
-		String b = "hard test1";
-		String c = "lol";
-		String d = "fuck)";
-		RunnerTask3.defineStringType(a);
-		RunnerTask3.defineStringType(b);
-		RunnerTask3.defineStringType(c);
-		RunnerTask3.defineStringType(d);
-		System.out.println();
+    //todo. Додати реалізацію для метода defineStringType метод повинен повертатти String в якому буде записано розмір стрінги.
+    // Якщо розмір стрінги менше 5 символів метод повинен повертнути "small", якщо від 5 до 10 символів то "medium", а якщо більше 10 символів то "big".
+    // Визнач розміри для змінних a, b, c, d, та виведи результат в консль у вигляді:"lol is a small string!"
+    // ** Створи нивий метод compareStrings, метод повинен отримувати 2 стрінги в якості аргументів методу і надрукувати результат порівняння
+    // наприклад: "String test less then string test2"
+    public static void main(String[] args) {
+        String a = "testing";
+        String b = "hard test1";
+        String c = "lol";
+        String d = "fuck)";
+        RunnerTask3.defineStringType(a);
+        RunnerTask3.defineStringType(b);
+        RunnerTask3.defineStringType(c);
+        RunnerTask3.defineStringType(d);
+        System.out.println();
 
-		System.out.println(a + " is the " +  "string!");
-		System.out.println();
-		System.out.println();
-		System.out.println();
+        System.out.println(a + " is the " + "string!");
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
-		compare("Toha","Antoha");
-
-
+        compare("Toha", "Toha");
 
 
+    }
 
+    private static void defineStringType(String string) {
+        String word = "Длина строки " + string + ": " + string.length();
+        String size = "";
+        if (string.length() < 5) {
+            size="small";
+        } else if (string.length() >= 5 & string.length() <= 10) {
+            size="medium";
+        } else if (string.length() > 10) {
+            size="big";
+        }
+        String result = word + " a розмер: " + size;
+        System.out.println(result);
 
-	}
+    }
 
-	private static String defineStringType(String string){
-		System.out.println("Длина строки "+string +": " + string.length());
-		if(string.length()<5){
-			System.out.println("small");
-		}else if (string.length()>=5 & string.length()<=10) {
-			System.out.println("medium");
-		}else if(string.length()>10){
-			System.out.println("big");
-			};
-		return string;
-
-	}
-	static void compare(String s,String s2){
-		if(s.compareTo(s2)>0){
-			System.out.println("s<s2");
-		}else System.out.println("s>s2");
-	}
+    static void compare(String s, String s2) {
+        if (s.length()>s2.length()) {
+            System.out.println("s<s2");
+        } else System.out.println("s>=s2");
+    }
 }
 
