78449914054fedfb54a0c426780907e1528e0534
maksym_bolshak <bolshakmv@gmail.com>
2022-04-10 00:02:22 +0300

    add task10

--- src/main/java/custom/level1/task10/RunnerTask10.java
+++ src/main/java/custom/level1/task10/RunnerTask10.java
@@ -1,6 +1,9 @@
 package custom.level1.task10;
 
 public class RunnerTask10 {
+    //todo. *** Create double array of String and init him from console and print him via for
+
+    //todo. *** Створи двомірний масив String та проініціалізуй його з консолі та надрукуй його за допомогою циклу for
     public static void main(String[] args) {
 
     }
